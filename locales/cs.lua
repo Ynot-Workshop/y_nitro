6b8e7d68706d7e2c5ad94ed30acc4a051e407252
stepanvalic <131240792+stepanvalic@users.noreply.github.com>
2024-01-05 18:01:01 +0100

    Create cs.lua

---
+++ locales/cs.lua
@@ -0,0 +1,11 @@
+{
+    "notify": {
+        "already_have_nos": "Už máš NOS!",
+        "not_in_vehicle": "Nejste ve vozidle",
+        "must_be_driver": "Musíte být řidičem!",
+        "canceled": "zrušeno": "Zrušeno.."
+    },
+    "progress": {
+        "connecting": "Připojení NOS.."
+    }
+}
