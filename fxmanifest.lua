a8341bf23743679c3064eb38cdd62ba9875211e1
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-01-05 03:36:35 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,22 +5,13 @@ description 'qbx_nitro'
 repository 'https://github.com/Qbox-project/qbx_nitro'
 version '1.0.0'
 
-shared_scripts {
-    '@ox_lib/init.lua',
-    '@qbx_core/shared/locale.lua',
-    'locales/en.lua',
-    'locales/*.lua',
-}
+shared_script '@ox_lib/init.lua'
 
 client_script 'client/main.lua'
 
 server_script 'server/main.lua'
 
-ui_page "html/index.html"
 files {
-	"html/index.html",
-    "html/style.css",
-    "html/script.js",
     "config/client.lua",
     "locales/*.json"
 }
